d860cb31fb8511b52e92763e2699d2112b564f74
JanoBourian <supermath33@icloud.com>
2023-01-03 21:51:36 -0600

    Add: I added dataclasses information

--- dataclasses/README.md
+++ dataclasses/README.md
@@ -3,3 +3,30 @@
 ## Relevant information about dataclasses
 
 - Has a *\_\_init\_\_()* method and *\_\_repr\_\_()* method by default
+
+## Arguments inside dataclasses decorator
+
+Before all, all parameters inside the decorator represents a specific dunder method.
+
+More information about dunder methods: https://levelup.gitconnected.com/python-dunder-methods-ea98ceabad15
+
+```python
+@dataclass(init=True, repr=True, eq=True, order=False, unsafe_hash=False, frozen=False,
+           match_args=True, kw_only=False, slots=False, weakref_slot=False)
+class C:
+    ...
+```
+## Arguments for dataclas decorator
+
+- init: 
+- repr: 
+- eq: To compare if two objects are equals
+- order: Boolean compairson with the next methods: it, le, gt, ge. 
+- unsafe_hash: \_\_hash\_\_()
+- frozen: setattr, delattr
+- match_args: match_args
+- kw_only:
+- slots: 
+- weakref_slot:
+
+## Arguments for fields
--- dataclasses/data_example.py
+++ dataclasses/data_example.py
@@ -1,4 +1,6 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+import random
+import string 
 
 @dataclass
 class InventoryItem:
@@ -10,4 +12,39 @@ class InventoryItem:
         return self.unit_price * self.quantity_on_hand
 
 item = InventoryItem("name", 5.9, 1)
+item_2 = InventoryItem("name", 5.9, 1)
+
+## __eq__()
+print(item.__eq__(item_2))
+print(item.total_cost())
 print(item)
+
+## Example to field
+@dataclass
+class Example:
+    mylist: list[int] = field(default_factory=list)
+
+c = Example()
+c.mylist += [1,2,3]
+print(c)
+print(c.mylist)
+
+## Other example
+
+def generate_id() -> str:
+    return "".join(random.choices(string.ascii_uppercase, k=12))
+    
+@dataclass(frozen = True, kw_only = True) # for no changes
+class Person:
+    name: str
+    address: str
+    id: str = field(init = False, default_factory=generate_id)
+    email_addresses: list[str] = field(default_factory = list)
+    active: bool = True
+    _search_string: str = field(init = False, repr = False)
+    
+    def __post_init__(self) -> None:
+        self._search_string = f"{self.name} {self.address}"
+
+p = Person(name = "john", address = "ravenue")
+print(p)
