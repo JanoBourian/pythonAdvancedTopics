c9f415364e4ba76603219f697901bf10dbeb05d0
JanoBourian <supermath33@hotmail.com>
2023-09-14 12:32:49 -0600

    Add: new way to consume async functions

--- asyncio/async_first.py
+++ asyncio/async_first.py
@@ -5,8 +5,10 @@ async def async_count(sleep_time:int):
     print(f"Time {sleep_time} seconds")
     await asyncio.sleep(sleep_time)
     print(f"Time {sleep_time} seconds")
+    return "janobourian"
     
 async def main():
+    task1, task2, task3 = None, None, None
     async with asyncio.TaskGroup() as tasks:
         print(f"Start at {time.strftime('%X')}")
         task1 = tasks.create_task(async_count(1))
@@ -14,5 +16,8 @@ async def main():
         task3 = tasks.create_task(async_count(3))
     print(f"Finish at {time.strftime('%X')}")
     print("Finished...")
+    print(task1.result())
+    print(task2.result())
+    print(task3.result())
 
 asyncio.run(main())
