2c7e8b51578c90ded29839834aa4ff75eab4745d
JanoBourian <supermath33@hotmail.com>
2023-10-06 16:51:05 -0600

    Variable naming best practices

--- clean_code/variables/variables.py
+++ clean_code/variables/variables.py
@@ -2,3 +2,43 @@ payment_method = "cash"
 order_quantity = 15
 order_Quantity = 25
 snake_case = "Laptop"
+
+# Firs rule
+ba_list = []
+bank_accounts = []
+
+# Second rule
+longest_word = 5
+longest_word_len = 5
+
+# Thrid rule
+a = "Gino"
+b = 5
+c = [1, 2, 3]
+
+# Fourth rule
+def conut_vowels(string):
+    allVowels = "aeiou"
+    total_count = 0
+    
+    for letter in string:
+        if letter in allVowels:
+            total_count += 1
+    
+    return total_count
+
+# Fifth rule
+gps_coordinates = [37.774933, -122.419416]
+number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+usa_population = 340264792
+
+# sixth rule
+plr_score = 500
+player_score = 500
+
+# seventh rule Plan ahead 
+
+# eigth rule
+total_price_of_all_items_in_the_cart = 5677
+total_price = 5677
+
