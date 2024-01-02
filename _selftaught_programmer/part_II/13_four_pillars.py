c1820af20d39f55c5e2a2af614ac9d35578f9721
JanoBourian <supermath33@hotmail.com>
2024-01-01 23:49:35 -0600

    Add: information about class encapsulation in Python

--- _selftaught_programmer/part_II/13_four_pillars.py
+++ _selftaught_programmer/part_II/13_four_pillars.py
@@ -1,6 +1,29 @@
+## Encapsulation
+
+class TopSecretValues:
+    
+    def __init__(self,name:str, secure_name:str):
+        self.name = name
+        self.__secure_name = secure_name
+    
+    def print_key(self):
+        print(self.__only_function_access())
+        return self.__secure_name
+    
+    def __only_function_access(self):
+        print("Super Secret Procedure")
+
+value = TopSecretValues("random", "SHA256")
+print(value.name)
+
+# print(value.__secure_name)
+# print(value.__only_function_access)
+print(value.print_key())
+
+
 ## Composition
 
-class Person():
+class Person:
     
     def __init__(self, name:str):
         self.name:str = name
