0f32323bb262cb47d39b6800bced5ef9c7dcc044
JanoBourian <supermath33@hotmail.com>
2024-01-06 00:29:35 -0600

    Add: Card Complete

--- _selftaught_programmer/part_II/15_war.py
+++ _selftaught_programmer/part_II/15_war.py
@@ -5,12 +5,32 @@ import random
 class Card:
     
     suits = ["spades", "hearts", "diamonds", "clubs"]
-    values = [None, None, "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+    values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
     
     def __init__(self):
         """ suit + values are ints """
         self.value = random.choice(self.values) # v
         self.suit = random.choice(self.suits) # s
+    
+    def __lt__(self, c2):
+        if self.value < c2.value:
+            return True
+        if self.value == c2.value:
+            if self.suit < c2.suit:
+                return True
+            else:
+                return False
+        return False
+    
+    def __gt__(self, c2):
+        if self.value > c2.value:
+            return True
+        if self.value == c2.value:
+            if self.suit > c2.suit:
+                return True
+            else:
+                return False
+        return False
     
     def __repr__(self):
         return f"{self.value} - {self.suit}"
@@ -22,7 +42,16 @@ class Card:
 # Game
 
 ## Testing
+print("__lt__ comparison")
 for _ in range(10):
-    print(Card())
+    card_1 = Card()
+    card_2 = Card()
+    print(f"{card_1} < {card_2}: {card_1 < card_2}")
+
+print("__gt__ comparison")
+for _ in range(10):
+    card_1 = Card()
+    card_2 = Card()
+    print(f"{card_1} > {card_2}: {card_1 > card_2}")
 
 ## All it together
