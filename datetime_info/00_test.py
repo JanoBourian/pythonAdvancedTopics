294ad925402b105696c6e67d4238b1fdd9efd66b
JanoBourian <supermath33@hotmail.com>
2024-01-11 00:12:47 -0600

    Add: some information about dates

--- datetime_info/00_test.py
+++ datetime_info/00_test.py
@@ -1,5 +1,13 @@
 import datetime 
 
-print(dir(datetime))
+print(dir(datetime.datetime))
 value_1 = "12-01-2024"
 value_2 = "2024-01-12"
+
+birthdate = datetime.datetime.strptime("2023-11-27 06:00:00", "%Y-%m-%d %H:%M:%S")
+print(birthdate)
+
+testing = datetime.datetime.strptime("2023-11-27 05:00:00", "%Y-%m-%d %H:%M:%S")
+print(testing)
+
+print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
